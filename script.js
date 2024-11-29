7ab54624f882e7e81cd713b01c65eaf567c6e0b5
Simoncastagne42 <simoncastagne42@gmail.com>
2024-11-29 16:39:53 +0100

    fin boutton ajouter

--- script.js
+++ script.js
@@ -1,31 +1,30 @@
 const addParticipant = document.querySelector("#ajouter");
-const input = document.createElement("input");
-      input.type = "text";
-      input.placeholder = "Saisissez un texte ici...";
-      input.id = "participantInput";
+const addButton = document.querySelector("#add");
+const participantsList = document.querySelector("#liste-participants");
+document.addEventListener("click", (event) => {
+    // Vérifie si le clic n'est ni sur l'input ni sur le bouton
+    if (
+      event.target !== addParticipant &&
+      event.target !== addButton
+    ) {
+      addParticipant.value = ""; // Réinitialise le champ
+    }
+  });
 
-      input.className = "styled-input";
 
-addParticipant.addEventListener("click",handleOnClickAjouter);
+  document.addEventListener("click",handleOnClickAdd) ;
+  function handleOnClickAdd() {
+    // Récupère la valeur de l'input
+    const valeur = addParticipant.value;
 
-function handleOnClickAjouter () {
     
-      addParticipant.replaceWith(input);
+        // Crée un nouvel élément de liste
+        const listParticipants = document.createElement("ul");
+        listParticipants.textContent = valeur;
+        listParticipants.className = "Liste-Participants";
 
-      // Ajoute un gestionnaire pour capturer l'entrée utilisateur lorsqu'il termine
-      input.addEventListener("blur", () => {
-        const valeur = input.value;
-
-        // Remet le bouton si aucune valeur n'est saisie
-        addParticipant.textContent = "Ajouter quelqu'un";
-        input.replaceWith(addParticipant); // Réinjecte le bouton
-      });
-
-      // Remplace le bouton par le champ de saisie
-      addParticipant.replaceWith(input);
-
-      // Donne immédiatement le focus au champ de saisie (= permet de saisir le texte directement sans avoir a reclické)
-      input.focus();
-    };
-
-  
+        // Ajoute l'élément à la liste
+        participantsList.appendChild(listParticipants);
+        // rénitialise le champ
+        addParticipant.value = "";
+  }
